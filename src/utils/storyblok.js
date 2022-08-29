d8d5d97353b03cf1fe735af138b5d95c83736dfa
Nicolae Bozbici <nicolaebozbici@Nicolaes-MacBook-Pro.local>
2022-08-29 13:00:00 +0300

    Integrated storyblock

---
+++ src/utils/storyblok.js
@@ -0,0 +1,37 @@
+import {
+  apiPlugin,
+  loadStoryblokBridge,
+  storyblokEditable,
+  storyblokInit,
+} from "@storyblok/js";
+
+const dev = "SbnLzPOG17Qmhc2YhirImwtt";
+const storyblokToken = "SbnLzPOG17Qmhc2YhirImwtt";
+
+const { storyblokApi } = storyblokInit({
+  accessToken: storyblokToken,
+  apiOptions: {
+    cache: { type: "memory" },
+  },
+  use: [apiPlugin],
+});
+
+export async function getLinks() {
+  const { data } = await storyblokApi.get("cdn/links", {
+    version: "draft",
+  });
+
+  const links = data ? data.links : null;
+
+  return links;
+}
+
+export async function getStory(slug) {
+  const { data } = await storyblokApi.get(`cdn/stories/${slug}`, {
+    version: "draft",
+  });
+
+  const story = data ? data.story : null;
+
+  return story;
+}
